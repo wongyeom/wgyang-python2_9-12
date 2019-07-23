494d01b336c029b782638c56426cff79ec18df28
cwksebastian2198 <student+2198@codingwithkids.com>
2019-07-23 09:42:28 -0700

    day 1

--- pygames.py
+++ pygames.py
@@ -2,16 +2,23 @@ import sys,pygame
 from pygame. locals import *
 
 pygame.init()
-screen_info = pygame.dusplay.info()
+screen_info = pygame.display.Info()
 
-screen = pygame.display.set_model(int(screen_info.current_w),int(screen_info.current_h))
-clock = pygame.time.clock()
+screen = pygame.display.set_mode((int(screen_info.current_w), int(screen_info.current_h)))
+clock = pygame.time.Clock()
+color = (200, 0, 200)
+
+fish_image = pygame.image.load("fish.png")
+
+fish_rect = fish_image.get_ret()
 
 def main():
     while True:
         clock.tick(60)
         screen.fill(color)
         pygame.display .flip()
+
+
 if __name__ == '__main__':
     main()
 
